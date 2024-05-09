2f2885b7da137ba0ff01b0320c0be919eb072cb8
saawaahh <112594593+saawaahh@users.noreply.github.com>
2024-05-09 18:01:47 -0500

    tuples
    ordered, unchangeable data

--- 02/02_06/02_06e/main.py
+++ 02/02_06/02_06e/main.py
@@ -1,15 +1,27 @@
 # Tuples are immutable array-like structures
+# Tuples cannot be changed 
 
 point = (5, 2)
 
-x = point[0]
-y = point[1]
+y = point[0]
+x = point[1]
 
+
+# tuples can also be used to retun multiple values 
+# here lets use that concept to calculate the area and perimeter of a squaree
 def calculate_square_properties(side_length):
-    area = side_length * side_length
-    perimeter = 4 * side_length
-    return (area, perimeter)
+  area = side_length * side_length
+  perimeter = 4 * side_length
 
-result = calculate_square_properties(5)
+  return(area, perimeter) # here we use a tuple to return both 
+
+
+# lets build on that more
+result = calculate_square_properties(5) # lets use an example of a square side length 5
 print("Area: ", result[0])
-print("Perimeter ", result[1])
+print("Perimeter: ", result[1])
+
+
+
+
+
