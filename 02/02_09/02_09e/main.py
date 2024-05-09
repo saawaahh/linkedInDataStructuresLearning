f1b1a7f98bcacff6caaf1ddd2c1db5ddae52db47
saawaahh <112594593+saawaahh@users.noreply.github.com>
2024-05-09 18:41:45 -0500

    challenge - find second smallest in list
    i sorted it then returned the second item

--- 02/02_09/02_09e/main.py
+++ 02/02_09/02_09e/main.py
@@ -1,4 +1,7 @@
+# i am supposed to find the second smallest number in the list
+
 def find_second_smallest(my_list):
-    return 0
+    sorted_list = sorted(my_list)
+    return sorted_list[1]
 
 print(find_second_smallest([5, 8, 3, 2, 6]))
