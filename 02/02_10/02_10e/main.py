26bdbc5bb4a480ff675f176f045687add0df7ad0
saawaahh <112594593+saawaahh@users.noreply.github.com>
2024-05-09 18:56:42 -0500

    challenge walkthrough

--- 02/02_10/02_10e/main.py
+++ 02/02_10/02_10e/main.py
@@ -1,20 +1,33 @@
+# i am supposed to find the second smallest number in the list
+
+# timesort? n log n 
 def find_second_smallest(my_list):
     if len(my_list) < 2:
-        return None
+        return None             # here is returns none if the its empty, but idk why its < 2 maybe bc second big? idkk 
     sorted_list = sorted(my_list)
     return sorted_list[1]
 
+
+# second option is to create a custom algorithm 
+# here we are going to iterate through the list keeping track of the two smallest items
+# O(n) bc we only iterate through the list once
 def find_second_smallest_v2(my_list):
     if len(my_list) < 2:
         return None
+    
     smallest = float('inf')
-    second_smallest = float('inf')
-    for num in my_list:
+    second_smallest = float('inf')          # i guess here we are making it big knowing that we will be overriding as we go 
+
+    for num in my_list: 
         if num < smallest:
             second_smallest = smallest
-            smallest = num
+            smallest = num              # oh okay i see lol 
         elif num < second_smallest:
             second_smallest = num
     return second_smallest
 
+
+print(find_second_smallest([5, 8, 3, 2, 6]))
 print(find_second_smallest_v2([5, 8, 3, 2, 6]))
+
+
