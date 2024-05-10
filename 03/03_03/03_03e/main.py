4a2c9934db01c7dd03d5204957e0f638597cc8b3
saawaahh <112594593+saawaahh@users.noreply.github.com>
2024-05-09 19:36:55 -0500

    mutate a dictionary

--- 03/03_03/03_03e/main.py
+++ 03/03_03/03_03e/main.py
@@ -1,3 +1,5 @@
+# mutating a dictionary 
+
 user_preferences = {
     "language": "English",
     "font_size": "14px",
@@ -8,12 +10,21 @@ user_preferences = {
     "date_format": "MM/DD/YYYY"
 }
 
+# lets change the language from english to spanish 
 user_preferences["language"] = "Spanish"
 
+#change the volume to 50, NOTEE: we are overriding here and can no longer access previous data
 user_preferences["volume_level"] = 50
-user_preferences["highlight_color"] = "yellow"
 
-del user_preferences["currency"]
-removed_item = user_preferences.pop("date_format", "N/A")
+# add an item to the dictionary 
+user_preferences["highlight_color"] = "pink"
+
+# delete an item from the dictionary 
+del user_preferences["currency"]            # the way it guessed currency for me really just lets me know codespaces is listining lol 
+
+# use the pop function to retrieve an item when you delete it
+# i think if i remember pop pops to the front? but a quick google search has me uncertain. something to ponder lol 
+removed_item = user_preferences.pop("date_format", "N/A")   # NA prevents a key error from occuring
+
 
 print(user_preferences)
