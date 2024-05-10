357e22a41afe239e65dcf4c42a626d25f76c7a98
saawaahh <112594593+saawaahh@users.noreply.github.com>
2024-05-09 19:18:41 -0500

    dictionaries
    abstract

--- 03/03_02/03_02e/main.py
+++ 03/03_02/03_02e/main.py
@@ -1,13 +1,19 @@
 # Key: State
 # Value: Capital
+# i think this kinda reminds me of a structure tbh
+# i guess this is abstract
 
 states_to_capitals = {
-    "Texas" : "Austin",
-    "New York" : "Albany"
+  "Texas" : "Austin",
+  "New York" : "Albany",
+  "Colorado" : "Denver"
 }
 
-print(states_to_capitals["New York"])
+print(states_to_capitals["Colorado"])
 
+for key in states_to_capitals.keys():
+  print(key)
+
+# now lets print out the key and its value 
 for key, value in states_to_capitals.items():
-    print(key + " | " + value)
-    
+  print(key + " | " + value)
