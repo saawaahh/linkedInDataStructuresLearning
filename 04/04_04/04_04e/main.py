85eae2ef100397f772e6d06b6662b9767030b120
saawaahh <112594593+saawaahh@users.noreply.github.com>
2024-05-09 21:15:56 -0500

    immutable/frozen sets

--- 04/04_04/04_04e/main.py
+++ 04/04_04/04_04e/main.py
@@ -1,6 +1,8 @@
-primary_colors = frozenset(["red", "blue", "yellow"])
+# making an immutable/frozen sett
+
+primary_colors = frozenset(["red", "blue", "yellow"])       # primary colors do not change so it makes sense to make it a frozen set
 
 if "blue" in primary_colors:
     print("Blue in the set!")
 
-# primary_colors.add("green")
+# primary_colors.add("green"). # this will throw an error bc its not in the set and the set cannot be changed so it cant be added
