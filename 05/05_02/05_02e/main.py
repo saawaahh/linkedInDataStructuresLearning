390bb30b8b1f10e72d17a107c9dd723df5988806
saawaahh <112594593+saawaahh@users.noreply.github.com>
2024-05-09 22:47:52 -0500

    de(queue)

--- 05/05_02/05_02e/main.py
+++ 05/05_02/05_02e/main.py
@@ -1,10 +1,15 @@
+# using a deque (pronounced deck) as a queue
+
 from collections import deque
 
 printer_queue = deque()
+
+# use append to add items to the queue
 printer_queue.append("TaylorSwiftTickets.pdf")
 printer_queue.append("MarketingNotes.docx")
 printer_queue.append("Proof.png")
 
+
 while len(printer_queue) > 0:
-    document = printer_queue.popleft()
+    document = printer_queue.popleft()  # printing out documents in a FIFO
     print(f'Printing {document}')
