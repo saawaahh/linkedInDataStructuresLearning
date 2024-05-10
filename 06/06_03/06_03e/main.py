c6b5c819a0fccd8a990aa86eae2f9bb809d54e0e
saawaahh <112594593+saawaahh@users.noreply.github.com>
2024-05-10 01:50:30 -0500

    deque as a stack

--- 06/06_03/06_03e/main.py
+++ 06/06_03/06_03e/main.py
@@ -1,9 +1,15 @@
+# use a deque as a stack 
+# here were saving browser history
+
 from collections import deque
 
-history_stack = deque()
-history_stack.append("https://google.com")
-history_stack.append("https://linkedin.com")
-history_stack.append("https://stackoverflow.com")
 
-print(history_stack[-1])
-print(history_stack.pop())
+history_stack = deque()   # create stack
+history_stack.append("https://www.myspace.com")
+history_stack.append("https://www.webkinz.com")
+history_stack.append("https://www.cats.com")
+
+print(history_stack[-1])      #does this without removing?
+print(history_stack.pop())    # prints the top of the stack
+
+
