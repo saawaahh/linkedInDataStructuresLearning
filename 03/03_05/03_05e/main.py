a559e2793292236497078767351286d5c338d2fb
saawaahh <112594593+saawaahh@users.noreply.github.com>
2024-05-09 19:58:22 -0500

    make a new dictionary using an existing dictionary
    / cleaning data

--- 03/03_05/03_05e/main.py
+++ 03/03_05/03_05e/main.py
@@ -6,12 +6,20 @@ user_preferences = {
     "theme": None
 }
 
+# clean our data 
 def update_preferences(user_pref):
-    return {key: value for key, value in user_pref.items() if value is not None }
-    # updated_preferences = {}
-    # for key, value in user_pref.items():
-    #     if value is not None:
-    #         updated_preferences[key] = value
-    # return updated_preferences
+    updated_preferences = {}
+    for key, value in user_pref.items():        #iterate through to find keys w no values
+        if value is not None:
+            updated_preferences[key] = value
+
+    return updated_preferences
+
+# do it using comprehension 
+def updatee_preferences(user_pref):
+    return {key: value for key, value in user_pref.items() if value is not None}
+
 
 print(update_preferences(user_preferences))
+print(updatee_preferences(user_preferences))
+
